57b4f5c7681da5e12ce778dbc6009a193680d9a6
mitelija08 <98547977+mitelija08@users.noreply.github.com>
2022-02-08 20:23:14 -0500

    add before hook for auth spec (#5)
    add root before hook

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,31 +1,50 @@
 import supertest from 'supertest'
 import { expect } from 'chai'
-import 'dotenv/config'
+
 
 describe('Auth', function () {
 
     const request = supertest(process.env.BASE_URL)
 
-    it('successful log in', function () {
+    describe('Successful log in', function() {
+        let result
+        before(async function () {
+            await request
+                .post('/auth')
+                .send({login: process.env.LOGIN, password: process.env.PASSWORD})
+                .then(res => {
+                    result = res
+                })
+        })
 
-        request
-            .post('/auth')
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(200)
-                expect(res.body.token).not.to.be.undefined
-            })
-    });
+        it('response status code is 200', function () {
+            expect(result.statusCode).to.eq(200)
+
+        });
+
+        it('response body contains authorization token', function () {
+            expect(result.body.token).not.to.be.undefined
+        })
+    })
 
 
-    it('Log in with invalid credentials', function () {
+    describe('Log in with invalid credentials', function() {
+    let result
 
-        request
+     before(async function() {
+        await request
             .post('/auth')
             .send({login: 'invalid', password: 'invalid'})
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(404)
-                expect(res.body.message).to.eq('Wrong login or password.')
+            .then(res => {
+                result = res
             })
-    });
+    })
+    it('response status code is 404', function () {
+        expect(result.statusCode).to.eq(404)
+    })
+
+    it('response body contains error message', function () {
+        expect(result.body.message).to.eq('Wrong login or password.')
+    })
+});
 })
